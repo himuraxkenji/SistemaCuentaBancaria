6f16e602c7f4b5d469106cea93f62a06ba6a16bc
Riperto Adriel Aaron <aaron.ariperto@gmail.com>
2019-06-11 19:34:43 -0300

    Se creo proyecto maven

---
+++ sistemaBancario/src/main/java/ar/edu/undec/sistemaBancario/App.java
@@ -0,0 +1,13 @@
+package ar.edu.undec.sistemaBancario;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
