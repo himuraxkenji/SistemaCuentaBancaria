15da88733ed66580404c102fdaa22af8810799bd
Riperto Adriel Aaron <aaron.ariperto@gmail.com>
2019-06-14 14:27:34 -0300

    Se Hizo el test atemporal para la fecha de nacimiento

--- sistemaBancario/src/test/java/ar/edu/undec/entidades/ClienteUnitTest.java
+++ sistemaBancario/src/test/java/ar/edu/undec/entidades/ClienteUnitTest.java
@@ -5,13 +5,21 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.exceptions.base.MockitoException;
 
 import ar.edu.undec.excepciones.ClienteException;
 import ar.edu.undec.excepciones.ClienteIncompletoException;
 import ar.edu.undec.excepciones.ClienteMenorEdadException;
+import ar.edu.undec.sistemaBancario.MockitoExtension;
 
+
+
+@ExtendWith(MockitoExtension.class)
 class ClienteUnitTest {
-
+	
+	
 	@Test
 	public void factoryCliente_TodoslosAtributosCorrectos_InstanciaCorrecta() throws ClienteException{
 		LocalDate fechaDeNacimiento = LocalDate.of(1993, 7, 10);
@@ -27,7 +35,10 @@ class ClienteUnitTest {
 	
 	@Test
 	public void factoryCliente_EdadMenorA18_InstanciaIncompleta() {
-		LocalDate fechaDeNacimiento = LocalDate.of(2005, 7, 10);
+		int treceAnios = LocalDate.now().getYear() - 14;
+		LocalDate fechaDeNacimiento = LocalDate.of(treceAnios, 7, 10);
+		
+		
 		assertThrows(ClienteMenorEdadException.class, () -> Cliente.factoryCliente("Ikuma Shibutani", "91462524", "Thead 1980" , "660-879-6425", fechaDeNacimiento));
 	}
 		
