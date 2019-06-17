f55ab7edc2e50f7586823af86351c28d39489cea
Riperto Adriel Aaron <aaron.ariperto@gmail.com>
2019-06-17 14:03:27 -0300

    Test atemporal de la clase cliente

--- sistemaBancario/src/test/java/ar/edu/undec/entidades/ClienteUnitTest.java
+++ sistemaBancario/src/test/java/ar/edu/undec/entidades/ClienteUnitTest.java
@@ -9,6 +9,7 @@ import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.exceptions.base.MockitoException;
 import ar.edu.undec.excepciones.ClienteException;
@@ -19,42 +20,16 @@ import ar.edu.undec.interfaces.IDateTime;
 import ar.edu.undec.sistemaBancario.MockitoExtension;
 
 
-
+@ExtendWith(MockitoExtension.class)
 class ClienteUnitTest {
 	
-	private IDateTime fechaSistema;
-
-    @Before
-    public void antesDeIniciar() {
-    	
-    	final LocalDate fecha = Mockito.mock(LocalDate.class);
-    	
-    	Mockito.when(fecha.now()).thenReturn(LocalDate.of(2019, 06, 19));
-    	
-    	final IDateTime fechaSistema = Mockito.mock(IDateTime.class);
-        
-    	Mockito.when(fechaSistema.getDate()).thenReturn(fecha);
-    	
-    	
-//    	final DateTimeImplementation fechaS = Mockito.mock(DateTimeImplementation.class);
-//    	
-//    	Mockito.when(fechaS.getDate()).thenReturn(LocalDate.of(2019, 06, 19));
-//    	
-//    	final IDateTime fecha = Mockito.mock(IDateTime.class);
-//    	
-//    	//final LocalDate fechaSis = Mockito.mock(DateTimeImplementation.class);
-//    	
-//    	Mockito.when(fecha.getDate()).thenReturn(fechaS.getDate());
-//        
-//    	//Mockito.when(fechaSis).thenReturn(fecha.getDate());
-//    	
-    	this.fechaSistema = fechaSistema ;
-    }
-
+	@Mock
+	IDateTime fechaSistema;
 	
 	@Test
 	public void factoryCliente_TodoslosAtributosCorrectos_InstanciaCorrecta() throws ClienteException{
 		LocalDate fechaDeNacimiento = LocalDate.of(1993, 7, 10);
+		Mockito.when(fechaSistema.getDate()).thenReturn(LocalDate.of(2019, 06, 19));
 		Cliente unCliente = Cliente.factoryCliente(fechaSistema,"Sakika", "91479524", "Quality 4929" , "904-735-7945", fechaDeNacimiento);
 		assertNotNull(unCliente);
 	}
@@ -62,6 +37,8 @@ class ClienteUnitTest {
 	@Test
 	public void factoryCliente_FaltanAtributos_InstanciaIncompleta() {
 		LocalDate fechaDeNacimiento = null;
+		Mockito.when(fechaSistema.getDate()).thenReturn(LocalDate.of(2019, 06, 19));
+		
 		assertThrows(ClienteIncompletoException.class, () -> Cliente.factoryCliente(fechaSistema,"", "", "" , "", fechaDeNacimiento));
 	}
 	
@@ -69,6 +46,7 @@ class ClienteUnitTest {
 	public void factoryCliente_EdadMenorA18_InstanciaIncompleta() {
 		
 		LocalDate fechaDeNacimiento = LocalDate.of(2013, 7, 10);
+		Mockito.when(fechaSistema.getDate()).thenReturn(LocalDate.of(2019, 06, 19));
 				
 		assertThrows(ClienteMenorEdadException.class, () -> Cliente.factoryCliente(fechaSistema,"Ikuma Shibutani", "91462524", "Thead 1980" , "660-879-6425", fechaDeNacimiento));
 	}
